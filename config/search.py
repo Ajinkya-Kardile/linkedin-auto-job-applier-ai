59dd5a5c3c9e54cd5d4b56c79ceca75c6e0ed975
ajinkya <ajinkyak@inuxu.media>
2026-05-11 12:29:09 +0530

    added: added more block keywords in job search

--- config/search.py
+++ config/search.py
@@ -9,7 +9,7 @@ from config.schema.search_model import SearchModel
 # INSTANTIATE YOUR DATA HERE
 # ==========================================
 search_data = SearchModel(
-    search_terms=["Java Developer", "Java Backend Developer", "Spring Boot Developer"],
+    search_terms=["Java Developer", "Java Backend Developer", "Spring Boot Developer",  "Software Developer"],
     search_location="Pune",
     switch_number=30,
     randomize_search_order=False,
@@ -35,7 +35,7 @@ search_data = SearchModel(
                          ".NET","Microsoft", "Web", "ios", "HubSpot", "ServiceNow", "security", "Generative",
                          "Machine", "Artificial", "AI", "ML", "Share", "Infios", "Embedded", "test",
                          "cyber","power", "php", "Ruby", "QA", "Django", "flask", "fastapi", "pega", "intern", "automation",
-                         "database","GenAI", "CRM"],
+                         "database","GenAI", "CRM", "Quality", ],
     job_desc_bad_words=["US Citizen", "USA Citizen", "No C2C", "No Corp2Corp", ".NET", "Embedded Programming", "PHP",
                         "Ruby", "CNC"],
     about_company_bad_words=["Crossover"],
